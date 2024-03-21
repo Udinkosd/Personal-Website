939ea1c2e2ae8027b8765ee5f8ed1c9755c44490
Udinkosd <126738691+Udinkosd@users.noreply.github.com>
2024-03-22 04:25:02 +0800

    Fix

--- assets/script.js
+++ assets/script.js
@@ -16,12 +16,12 @@ updateCopyrightYear();
 
 function typingText() {
   const typingElement = document.querySelector(".auto-type");
-  const strings = ['Hello', 'Welcome To My Portfolio']; // Adjusted strings
-  const typeSpeed = 50; // Faster typing speed
-  const backSpeed = 80; // Slightly slower backspacing speed
-  const startDelay = 500; // Delay before typing starts
-  const backDelay = 1000; // Delay before backspacing starts
-  const loop = true;
+  const strings = ['Hello', 'Welcome To My Portfolio...'];
+  const typeSpeed = 50; 
+  const backSpeed = 80;
+  const startDelay = 500;
+  const backDelay = 1000;
+  const loop = false;
 
   const typed = new Typed(typingElement, {
     strings: strings,
@@ -34,29 +34,3 @@ function typingText() {
 }
 
 typingText();
-
-// function toggleAbout() {
-//     var aboutSection = document.getElementById('showAbout');
-    
-//     if (aboutSection.style.display === 'none') {
-//         aboutSection.style.display = 'block';
-//     } else {
-//         aboutSection.style.display = 'none';
-//     }
-// }
-
-// const showAboutSection = document.getElementById('showAbout');
-// const triggerPoint = 500; // Adjust this value as needed
-
-// window.addEventListener('scroll', function() {
-//     // Get the current scroll position
-//     const scrollPosition = window.scrollY || window.pageYOffset;
-
-//     // Check if the scroll position is beyond the trigger point
-//     if (scrollPosition > triggerPoint) {
-//         showAboutSection.classList.remove('skip-me'); // Show the about section
-//     } else {
-//         showAboutSection.classList.add('skip-me'); // Hide the about section
-//     }
-// });
-
